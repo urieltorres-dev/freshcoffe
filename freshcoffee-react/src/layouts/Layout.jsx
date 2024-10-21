d0e28811814e44666f80eff12550cf7321d007e6
Uriel Torres <urieltorres.dev@gmail.com>
2024-10-21 13:57:39 -0600

    Se creó el diseño de las vistas de autenticación

--- freshcoffee-react/src/layouts/Layout.jsx
+++ freshcoffee-react/src/layouts/Layout.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
 export default function Layout() {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <div>
+            Layout
+
+            <Outlet />
+        </div>
+    )
 }
--- freshcoffee-react/src/main.jsx
+++ freshcoffee-react/src/main.jsx
@@ -5,7 +5,7 @@ import router from './router'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
